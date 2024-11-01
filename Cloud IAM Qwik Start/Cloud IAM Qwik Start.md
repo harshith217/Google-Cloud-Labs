2e2ec2152403ee7158c222737a4ae2ccad863023
Arcade Crew <arcade8959@gmail.com>
2024-11-01 17:59:52 +0530

    Add files via upload

---
+++ Cloud IAM Qwik Start/Cloud IAM Qwik Start.md
@@ -0,0 +1,37 @@
+# 🌐 Cloud IAM: Qwik Start
+### 📖 Lab Guide: [GSP064](https://www.cloudskillsboost.google/focuses/44159?parent=catalog)
+
+--- 
+
+Watch the full video walkthrough for this lab:  
+[![YouTube Solution](https://img.shields.io/badge/YouTube-Watch%20Solution-red?style=flat&logo=youtube)](https://www.youtube.com/watch?v=wjSrI-UHmM8)
+
+---
+
+## 🛠️ Steps to Complete the Lab
+
+1. **Log in with Username 1** in the Google Cloud Console.
+
+2. **Run the following commands** in **Cloud Shell** (Be sure to copy `USERNAME 2`):
+
+    ```bash
+    curl -LO raw.githubusercontent.com/ArcadeCrew/Google-Cloud-Labs/refs/heads/main/Multiple%20VPC%20Networks/arcadecrew.sh
+
+    sudo chmod +x arcadecrew.sh
+
+    ./arcadecrew.sh
+    ```
+
+---
+
+### 🏆 Congratulations! You've completed the Lab! 🎉
+
+---
+
+### 🤝 Join the Community!
+
+- [Whatsapp Group](https://chat.whatsapp.com/FbVg9NI6Dp4CzfdsYmy0AE)  
+
+[![Arcade Crew Channel](https://img.shields.io/badge/YouTube-Arcade%20Crew-red?style=flat&logo=youtube)](https://www.youtube.com/@Arcade61432)
+
+---
---
+++ Cloud IAM Qwik Start/arcadecrew.sh
@@ -0,0 +1,39 @@
+# Define color codes for output formatting
+YELLOW_COLOR='\033[0;33m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=`tput setab 2`
+RED_TEXT=`tput setaf 1`
+
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+
+NO_COLOR='\033[0m'
+
+echo "${BACKGROUND_RED}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+
+# Prompt user for their username
+read -p "Enter the username to add/remove from IAM policy: " USERNAME_2
+
+# Set the storage bucket and enable uniform bucket-level access
+gsutil mb -l us -b on gs://$DEVSHELL_PROJECT_ID
+
+# Create a sample file and write content to it
+echo "Subscribe to Arcade Crew" > sample.txt
+
+# Upload the sample file to the specified bucket
+gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID
+
+# Remove IAM policy binding for the specified user
+gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USERNAME_2 \
+  --role=roles/viewer
+
+# Add IAM policy binding for storage object viewer role
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USERNAME_2 \
+  --role=roles/storage.objectViewer
+
+
+# Completion message
+echo -e "${RED_TEXT}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Check out our Channel: \e]8;;https://www.youtube.com/@Arcade61432\e\\https://www.youtube.com/@Arcade61432\e]8;;\e\\${RESET_FORMAT}"
