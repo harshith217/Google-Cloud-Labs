bede351dc0f6e66e1e8fd1bf37a2d008b56f126c
Arcade Crew <arcade8959@gmail.com>
2024-11-01 19:08:22 +0530

    Add files via upload

---
+++ Get Started with Eventarc Challenge Lab/arcadecrew.sh
@@ -0,0 +1,46 @@
+# Define color codes for output formatting
+YELLOW_COLOR=$'\033[0;33m'
+NO_COLOR=$'\033[0m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=`tput setab 2`
+RED_TEXT=`tput setaf 1`
+
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+
+echo "${BACKGROUND_RED}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+
+echo -e "${YELLOW_COLOR}${BOLD_TEXT}Enter the location:${RESET_FORMAT} \c"
+export LOCATION=$LOCATION
+
+# Enable required services on Google Cloud
+gcloud services enable run.googleapis.com
+gcloud services enable eventarc.googleapis.com
+
+# Set up Pub/Sub topic and subscription
+gcloud pubsub topics create "$DEVSHELL_PROJECT_ID-topic"
+gcloud pubsub subscriptions create --topic "$DEVSHELL_PROJECT_ID-topic" "$DEVSHELL_PROJECT_ID-topic-sub"
+
+# Deploy the Cloud Run service
+gcloud run deploy pubsub-events \
+  --image=gcr.io/cloudrun/hello \
+  --platform=managed \
+  --region="$LOCATION" \
+  --allow-unauthenticated
+
+# Create Eventarc trigger to listen to Pub/Sub messages
+gcloud eventarc triggers create pubsub-events-trigger \
+  --location="$LOCATION" \
+  --destination-run-service=pubsub-events \
+  --destination-run-region="$LOCATION" \
+  --transport-topic="$DEVSHELL_PROJECT_ID-topic" \
+  --event-filters="type=google.cloud.pubsub.topic.v1.messagePublished"
+
+# Send a test message to the Pub/Sub topic
+gcloud pubsub topics publish "$DEVSHELL_PROJECT_ID-topic" \
+  --message="Test message"
+
+
+# Completion message
+echo -e "${RED_TEXT}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Check out our Channel: \e]8;;https://www.youtube.com/@Arcade61432\e\\https://www.youtube.com/@Arcade61432\e]8;;\e\\${RESET_FORMAT}"
