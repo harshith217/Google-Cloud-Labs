0543c263fd94e6a3611127c487e715d8fc887a13
Arcade Crew <arcade8959@gmail.com>
2024-11-12 13:00:55 +0530

    Update arcadecrew.sh

--- Cloud Speech API 3 Ways Challenge Lab/arcadecrew.sh
+++ Cloud Speech API 3 Ways Challenge Lab/arcadecrew.sh
@@ -12,20 +12,6 @@ RESET_FORMAT=`tput sgr0`
 # Display initiation message
 echo "${BACKGROUND_RED}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
 
-#!/bin/bash
-
-# Define color variables
-YELLOW_COLOR=$'\033[0;33m'
-NO_COLOR=$'\033[0m'
-BACKGROUND_RED=`tput setab 1`
-GREEN_TEXT=`tput setab 2`
-RED_TEXT=`tput setaf 1`
-BOLD_TEXT=`tput bold`
-RESET_FORMAT=`tput sgr0`
-
-# Display initiation message
-echo "${BACKGROUND_RED}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
-
 audio_uri="gs://cloud-samples-data/speech/corbeau_renard.flac"
 
 export PROJECT_ID=$(gcloud config get-value project)
