1767f97b38a8bb344188053757bb1dfb7641ebc2
Arcade Crew <arcade8959@gmail.com>
2024-12-07 16:33:18 +0530

    Add files via upload

---
+++ Cloud Data Loss Prevention API Qwik Start/arcadecrew.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Define color variables
+YELLOW_COLOR=$'\033[0;33m'
+NO_COLOR=$'\033[0m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=`tput setab 2`
+RED_TEXT=`tput setaf 1`
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+BLUE_TEXT=`tput setaf 4`
+
+echo ""
+echo ""
+
+# Display initiation message
+echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+
+echo ""
+
+
+gcloud auth list
+
+cat > inspect-request.json <<EOF_END
+{
+  "item":{
+    "value":"My phone number is (206) 555-0123."
+  },
+  "inspectConfig":{
+    "infoTypes":[
+      {
+        "name":"PHONE_NUMBER"
+      },
+      {
+        "name":"US_TOLLFREE_PHONE_NUMBER"
+      }
+    ],
+    "minLikelihood":"POSSIBLE",
+    "limits":{
+      "maxFindingsPerItem":0
+    },
+    "includeQuote":true
+  }
+}
+EOF_END
+
+
+ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
+
+curl -s \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/content:inspect \
+  -d @inspect-request.json -o inspect-output.txt
+
+
+gsutil cp inspect-output.txt gs://$DEVSHELL_PROJECT_ID-bucket
+
+
+
+
+cat > new-inspect-file.json <<EOF_END
+{
+  "item": {
+     "value":"My email is test@gmail.com",
+   },
+   "deidentifyConfig": {
+     "infoTypeTransformations":{
+          "transformations": [
+            {
+              "primitiveTransformation": {
+                "replaceWithInfoTypeConfig": {}
+              }
+            }
+          ]
+        }
+    },
+    "inspectConfig": {
+      "infoTypes": {
+        "name": "EMAIL_ADDRESS"
+      }
+    }
+}
+EOF_END
+
+
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/content:deidentify \
+  -d @new-inspect-file.json -o redact-output.txt
+
+gsutil cp redact-output.txt gs://$DEVSHELL_PROJECT_ID-bucket
+
+echo ""
+# Completion message
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
+
