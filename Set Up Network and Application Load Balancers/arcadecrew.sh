b006e7107496b6ebd6db8415ef0b41d83922ce15
Arcade Crew <arcade8959@gmail.com>
2025-02-03 20:51:04 +0530

    Add files via upload

---
+++ Set Up Network and Application Load Balancers/arcadecrew.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+# Define color variables
+YELLOW_COLOR=$'\033[0;33m'
+MAGENTA_COLOR="\e[35m"
+NO_COLOR=$'\033[0m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=$'\033[0;32m'
+RED_TEXT=`tput setaf 1`
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+BLUE_TEXT=`tput setaf 4`
+
+echo ""
+echo ""
+
+# Display initiation message
+echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+
+echo
+
+read -p "${YELLOW}${BOLD}Enter the ZONE: ${RESET}" ZONE
+
+# Export variables after collecting input
+export ZONE
+
+export REGION=${ZONE%-*}
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+
+gcloud compute instances create www1 \
+  --zone=$ZONE \
+  --tags=network-lb-tag \
+  --machine-type=e2-small \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --metadata=startup-script='#!/bin/bash
+    apt-get update
+    apt-get install apache2 -y
+    service apache2 restart
+    echo "
+<h3>Web Server: www1</h3>" | tee /var/www/html/index.html'
+
+gcloud compute instances create www2 \
+  --zone=$ZONE \
+  --tags=network-lb-tag \
+  --machine-type=e2-small \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --metadata=startup-script='#!/bin/bash
+    apt-get update
+    apt-get install apache2 -y
+    service apache2 restart
+    echo "
+<h3>Web Server: www2</h3>" | tee /var/www/html/index.html'
+
+gcloud compute instances create www3 \
+  --zone=$ZONE  \
+  --tags=network-lb-tag \
+  --machine-type=e2-small \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --metadata=startup-script='#!/bin/bash
+    apt-get update
+    apt-get install apache2 -y
+    service apache2 restart
+    echo "
+<h3>Web Server: www3</h3>" | tee /var/www/html/index.html'
+
+gcloud compute firewall-rules create www-firewall-network-lb \
+    --target-tags network-lb-tag --allow tcp:80
+
+gcloud compute instances list
+
+gcloud compute addresses create network-lb-ip-1 \
+  --region $REGION
+
+gcloud compute http-health-checks create basic-check
+
+gcloud compute target-pools create www-pool \
+  --region $REGION --http-health-check basic-check
+
+gcloud compute target-pools add-instances www-pool \
+    --instances www1,www2,www3
+
+gcloud compute forwarding-rules create www-rule \
+    --region  $REGION \
+    --ports 80 \
+    --address network-lb-ip-1 \
+    --target-pool www-pool
+
+gcloud compute forwarding-rules describe www-rule --region $REGION
+
+IPADDRESS=$(gcloud compute forwarding-rules describe www-rule --region $REGION --format="json" | jq -r .IPAddress)
+
+echo $IPADDRESS
+
+gcloud compute instance-templates create lb-backend-template \
+   --region=$REGION \
+   --network=default \
+   --subnet=default \
+   --tags=allow-health-check \
+   --machine-type=e2-medium \
+   --image-family=debian-11 \
+   --image-project=debian-cloud \
+   --metadata=startup-script='#!/bin/bash
+     apt-get update
+     apt-get install apache2 -y
+     a2ensite default-ssl
+     a2enmod ssl
+     vm_hostname="$(curl -H "Metadata-Flavor:Google" \
+     http://169.254.169.254/computeMetadata/v1/instance/name)"
+     echo "Page served from: $vm_hostname" | \
+     tee /var/www/html/index.html
+     systemctl restart apache2'
+
+gcloud compute instance-groups managed create lb-backend-group \
+   --template=lb-backend-template --size=2 --zone=$ZONE
+
+gcloud compute firewall-rules create fw-allow-health-check \
+  --network=default \
+  --action=allow \
+  --direction=ingress \
+  --source-ranges=130.211.0.0/22,35.191.0.0/16 \
+  --target-tags=allow-health-check \
+  --rules=tcp:80
+
+gcloud compute addresses create lb-ipv4-1 \
+  --ip-version=IPV4 \
+  --global
+
+gcloud compute addresses describe lb-ipv4-1 \
+  --format="get(address)" \
+  --global
+
+gcloud compute health-checks create http http-basic-check \
+  --port 80
+
+gcloud compute backend-services create web-backend-service \
+  --protocol=HTTP \
+  --port-name=http \
+  --health-checks=http-basic-check \
+  --global
+
+gcloud compute backend-services add-backend web-backend-service \
+  --instance-group=lb-backend-group \
+  --instance-group-zone=$ZONE \
+  --global
+
+gcloud compute url-maps create web-map-http \
+    --default-service web-backend-service
+
+gcloud compute target-http-proxies create http-lb-proxy \
+    --url-map web-map-http
+
+gcloud compute forwarding-rules create http-content-rule \
+   --address=lb-ipv4-1\
+   --global \
+   --target-http-proxy=http-lb-proxy \
+   --ports=80
+
+echo
+echo -e "\e[1;31mDeleting the script (arcadecrew.sh) for safety purposes...\e[0m"
+rm -- "$0"
+echo
+echo
+# Completion message
+echo -e "${MAGENTA_COLOR}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
+echo
