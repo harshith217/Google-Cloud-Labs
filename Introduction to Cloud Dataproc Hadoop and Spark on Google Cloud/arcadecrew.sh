d59354e0de35d2091c65d072d0259addc62047c3
Arcade Crew <arcade8959@gmail.com>
2025-02-05 19:50:25 +0530

    Update arcadecrew.sh

--- Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/arcadecrew.sh
+++ Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/arcadecrew.sh
@@ -22,14 +22,12 @@ echo
 read -p "${YELLOW_COLOR}${BOLD_TEXT}Enter CLUSTER_NAME: ${RESET_FORMAT}" CLUSTER_NAME
 
 # Export variables after collecting input
-export CLUSTER_NAME 
+export CLUSTER_NAME
 
 gcloud auth list
 
 export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-
 export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
-
 
 PROJECT_NUMBER=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
 
@@ -37,8 +35,43 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
     --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
     --role="roles/storage.admin"
 
-gcloud dataproc clusters create $CLUSTER_NAME --project $DEVSHELL_PROJECT_ID --region $REGION --zone $ZONE --master-machine-type n1-standard-2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 100GB --worker-boot-disk-type pd-standard --num-workers 2 --no-address
+# Retry loop for cluster creation
+echo -e "\e[1;34mStarting cluster creation...\e[0m"
+while true; do
+    gcloud dataproc clusters create $CLUSTER_NAME \
+        --project $DEVSHELL_PROJECT_ID \
+        --region $REGION \
+        --zone $ZONE \
+        --master-machine-type n1-standard-2 \
+        --worker-machine-type n1-standard-2 \
+        --worker-boot-disk-size 100GB \
+        --worker-boot-disk-type pd-standard \
+        --num-workers 2 \
+        --no-address
 
+    if [[ $? -eq 0 ]]; then
+        echo -e "\e[1;32mCluster creation successful!\e[0m"
+        break
+    else
+        echo -e "\e[1;31mCluster creation failed. Retrying in 10 seconds...\e[0m"
+        sleep 10
+    fi
+done
+
+# Wait until cluster is in a RUNNING state
+echo -e "\e[1;34mWaiting for the cluster to be in RUNNING state...\e[0m"
+while true; do
+    STATUS=$(gcloud dataproc clusters describe $CLUSTER_NAME --region $REGION --format="value(status.state)")
+    if [[ "$STATUS" == "RUNNING" ]]; then
+        echo -e "\e[1;32mCluster is now RUNNING!\e[0m"
+        break
+    else
+        echo -e "\e[1;33mCurrent status: $STATUS. Retrying in 10 seconds...\e[0m"
+        sleep 10
+    fi
+done
+
+# Submit Spark job
 gcloud dataproc jobs submit spark \
     --project $DEVSHELL_PROJECT_ID \
     --region $REGION \
@@ -46,7 +79,6 @@ gcloud dataproc jobs submit spark \
     --class org.apache.spark.examples.SparkPi \
     --jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
     -- 1000
-
 
 echo "Click this link to open" "${BLUE_TEXT}${BOLD_TEXT}https://console.cloud.google.com/dataproc/jobs?project=$DEVSHELL_PROJECT_ID${RESET_FORMAT}"
 
