50317c35b0cc1be37c5c50c17bacba72507b6073
Arcade Crew <arcade8959@gmail.com>
2025-02-05 20:44:47 +0530

    Update arcadecrew.sh

--- Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/arcadecrew.sh
+++ Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/arcadecrew.sh
@@ -1,64 +1,62 @@
 #!/bin/bash
 
-# Define color variables
+# Define color variables for output formatting
 YELLOW_COLOR=$'\033[0;33m'
 MAGENTA_COLOR="\e[35m"
 NO_COLOR=$'\033[0m'
-BACKGROUND_RED=`tput setab 1`
 GREEN_TEXT=$'\033[0;32m'
-RED_TEXT=`tput setaf 1`
-BOLD_TEXT=`tput bold`
-RESET_FORMAT=`tput sgr0`
-BLUE_TEXT=`tput setaf 4`
+RED_TEXT=$'\033[0;31m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
+BLUE_TEXT=$'\033[0;34m'
 
 echo
+echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Cloud Skills Boost Lab Automation...${RESET_FORMAT}"
 echo
 
-# Display initiation message
-echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
-
-echo
-
+# Set default values
 read -p "${YELLOW_COLOR}${BOLD_TEXT}Enter CLUSTER_NAME: ${RESET_FORMAT}" CLUSTER_NAME
 
 # Export variables after collecting input
 export CLUSTER_NAME
-
-gcloud auth list
-
 export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+MASTER_MACHINE_TYPE="e2-standard-4"
+WORKER_MACHINE_TYPE="e2-standard-2"
+NUM_WORKERS=2
+JOB_TYPE="spark"
+MAIN_CLASS="org.apache.spark.examples.SparkPi"
+JAR_FILE="file:///usr/lib/spark/examples/jars/spark-examples.jar"
+ARGUMENTS=1000
 
-PROJECT_NUMBER=$(gcloud projects describe $(gcloud config get-value project) --format="value(projectNumber)")
+# Export variables
+export CLUSTER_NAME REGION ZONE MASTER_MACHINE_TYPE WORKER_MACHINE_TYPE NUM_WORKERS JOB_TYPE MAIN_CLASS JAR_FILE ARGUMENTS
 
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+# Authenticate and set project details
+gcloud auth list
+
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+
+# Ensure IAM permissions are set
+gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
     --role="roles/storage.admin"
 
-# Retry loop for cluster creation
-echo -e "\e[1;34mStarting cluster creation...\e[0m"
-while true; do
-    gcloud dataproc clusters create $CLUSTER_NAME \
-        --project $DEVSHELL_PROJECT_ID \
-        --region $REGION \
-        --zone $ZONE \
-        --master-machine-type n1-standard-2 \
-        --worker-machine-type n1-standard-2 \
-        --worker-boot-disk-size 100GB \
-        --worker-boot-disk-type pd-standard \
-        --num-workers 2 \
-        --no-address
+# Create Dataproc cluster
+echo -e "\e[1;34mCreating Dataproc cluster: $CLUSTER_NAME...\e[0m"
+gcloud dataproc clusters create $CLUSTER_NAME \
+    --project $PROJECT_ID \
+    --region $REGION \
+    --zone $ZONE \
+    --master-machine-type $MASTER_MACHINE_TYPE \
+    --worker-machine-type $WORKER_MACHINE_TYPE \
+    --worker-boot-disk-size 100GB \
+    --worker-boot-disk-type pd-standard \
+    --num-workers $NUM_WORKERS \
+    --no-address
 
-    if [[ $? -eq 0 ]]; then
-        echo -e "\e[1;32mCluster creation successful!\e[0m"
-        break
-    else
-        echo -e "\e[1;31mCluster creation failed. Retrying in 10 seconds...\e[0m"
-        sleep 10
-    fi
-done
-
-# Wait until cluster is in a RUNNING state
+# Wait for the cluster to be in RUNNING state
 echo -e "\e[1;34mWaiting for the cluster to be in RUNNING state...\e[0m"
 while true; do
     STATUS=$(gcloud dataproc clusters describe $CLUSTER_NAME --region $REGION --format="value(status.state)")
@@ -72,22 +70,20 @@ while true; do
 done
 
 # Submit Spark job
-gcloud dataproc jobs submit spark \
-    --project $DEVSHELL_PROJECT_ID \
+echo -e "\e[1;34mSubmitting Spark job...\e[0m"
+gcloud dataproc jobs submit $JOB_TYPE \
+    --project $PROJECT_ID \
     --region $REGION \
     --cluster $CLUSTER_NAME \
-    --class org.apache.spark.examples.SparkPi \
-    --jars file:///usr/lib/spark/examples/jars/spark-examples.jar \
-    -- 1000
+    --class $MAIN_CLASS \
+    --jars $JAR_FILE \
+    -- $ARGUMENTS
 
-echo "Click this link to open" "${BLUE_TEXT}${BOLD_TEXT}https://console.cloud.google.com/dataproc/jobs?project=$DEVSHELL_PROJECT_ID${RESET_FORMAT}"
+echo -e "Click this link to open ${BLUE_TEXT}${BOLD_TEXT}https://console.cloud.google.com/dataproc/jobs?project=$PROJECT_ID${RESET_FORMAT}"
 
-echo
-echo -e "\e[1;31mDeleting the script (arcadecrew.sh) for safety purposes...\e[0m"
+echo -e "\e[1;31mDeleting the script (dataproc_lab.sh) for safety purposes...\e[0m"
 rm -- "$0"
-echo
-echo
+
 # Completion message
 echo -e "${MAGENTA_COLOR}IF GETTING ${BOLD_TEXT}ERROR RERUN THE COMMANDS.${RESET_FORMAT}"
-echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
-echo
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe to our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
