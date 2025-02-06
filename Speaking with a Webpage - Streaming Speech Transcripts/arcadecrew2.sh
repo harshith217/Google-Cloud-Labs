a5e8712955f77f6336c654b202d33279a6ebacee
Arcade Crew <arcade8959@gmail.com>
2025-02-06 18:15:28 +0530

    Update arcadecrew2.sh

--- Speaking with a Webpage - Streaming Speech Transcripts/arcadecrew2.sh
+++ Speaking with a Webpage - Streaming Speech Transcripts/arcadecrew2.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Define color variables
-YELLOW_COLOR=$'\033[0;33m'
-MAGENTA_COLOR="\e[35m"
-NO_COLOR=$'\033[0m'
-BACKGROUND_RED=`tput setab 1`
-GREEN_TEXT=$'\033[0;32m'
-RED_TEXT=`tput setaf 1`
-BOLD_TEXT=`tput bold`
-RESET_FORMAT=`tput sgr0`
-BLUE_TEXT=`tput setaf 4`
-
-echo
-echo
+YELLOW_COLOR='\033[0;33m'
+MAGENTA_COLOR='\033[0;35m'
+NO_COLOR='\033[0m'
+BACKGROUND_RED='\033[41m'
+GREEN_TEXT='\033[0;32m'
+RED_TEXT='\033[0;31m'
+BOLD_TEXT='\033[1m'
+RESET_FORMAT='\033[0m'
+BLUE_TEXT='\033[0;34m'
+CYAN_TEXT='\033[0;36m'
 
 # Display initiation message
-echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
 
 echo
 
@@ -24,6 +22,7 @@ export ZONE=$(gcloud compute instances list speaking-with-a-webpage --format 'cs
 export VM_EXT_IP=$(gcloud compute instances describe speaking-with-a-webpage --zone=$ZONE \
   --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 
-echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}""https://$VM_EXT_IP:8443"""${RESET}"
+# Display clickable link
+echo -e "${CYAN_TEXT}${BOLD_TEXT}Click here: ${RESET_FORMAT}${BLUE_TEXT}${BOLD_TEXT}https://$VM_EXT_IP:8443${RESET_FORMAT}"
 
 echo
