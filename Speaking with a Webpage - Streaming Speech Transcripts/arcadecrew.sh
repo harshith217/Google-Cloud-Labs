15c9f2ca60fe4b7b255066b59e25ff84ddc2eb29
Arcade Crew <arcade8959@gmail.com>
2025-02-06 18:16:49 +0530

    Update arcadecrew.sh

--- Speaking with a Webpage - Streaming Speech Transcripts/arcadecrew.sh
+++ Speaking with a Webpage - Streaming Speech Transcripts/arcadecrew.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Define color variables
-YELLOW_COLOR=$'\033[0;33m'
-MAGENTA_COLOR="\e[35m"
-NO_COLOR=$'\033[0m'
-BACKGROUND_RED=`tput setab 1`
-GREEN_TEXT=$'\033[0;32m'
-RED_TEXT=`tput setaf 1`
-BOLD_TEXT=`tput bold`
-RESET_FORMAT=`tput sgr0`
-BLUE_TEXT=`tput setaf 4`
-
-echo
-echo
+YELLOW_COLOR='\033[0;33m'
+MAGENTA_COLOR='\033[0;35m'
+NO_COLOR='\033[0m'
+BACKGROUND_RED='\033[41m'
+GREEN_TEXT='\033[0;32m'
+RED_TEXT='\033[0;31m'
+BOLD_TEXT='\033[1m'
+RESET_FORMAT='\033[0m'
+BLUE_TEXT='\033[0;34m'
+CYAN_TEXT='\033[0;36m'
 
 # Display initiation message
-echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
 
 echo
 
@@ -25,17 +23,8 @@ sleep 5
 
 gcloud compute ssh "speaking-with-a-webpage" --zone "$ZONE" --project "$DEVSHELL_PROJECT_ID" --quiet --command "cd ~/speaking-with-a-webpage/02-webaudio && mvn clean jetty:run"
 
-# echo
-# # Safely delete the script if it exists
-# SCRIPT_NAME="arcadecrew.sh"
-# if [ -f "$SCRIPT_NAME" ]; then
-#     echo -e "${BOLD_TEXT}${RED_TEXT}Deleting the script ($SCRIPT_NAME) for safety purposes...${RESET_FORMAT}${NO_COLOR}"
-#     rm -- "$SCRIPT_NAME"
-# fi
+echo
 
-echo
-echo
-# Completion message
 echo -e "${MAGENTA_COLOR}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
-echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe to our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
 echo
