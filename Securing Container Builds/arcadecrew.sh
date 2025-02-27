57f359891738685101fa09916dedc4c4fa0746d2
Arcade Crew <arcade8959@gmail.com>
2025-02-27 17:46:21 +0530

    Update arcadecrew.sh

--- Securing Container Builds/arcadecrew.sh
+++ Securing Container Builds/arcadecrew.sh
@@ -22,9 +22,6 @@ echo "${CYAN_TEXT}${BOLD_TEXT}                  Starting the process...         
 echo "${CYAN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo
 
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 1: Retrieving Project Information                    ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Retrieving Project ID, Project Number, Zone, and Region...${RESET_FORMAT}"
 
 export ZONE=$(gcloud compute project-info describe \
@@ -39,9 +36,6 @@ echo "${GREEN_TEXT}Zone: ${ZONE}${RESET_FORMAT}"
 echo "${GREEN_TEXT}Region: ${REGION}${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔═════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 2: Enabling Artifact Registry API                   ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚═════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Enabling the Artifact Registry API...${RESET_FORMAT}"
 
 gcloud services enable artifactregistry.googleapis.com
@@ -49,9 +43,6 @@ gcloud services enable artifactregistry.googleapis.com
 echo "${GREEN_TEXT}Artifact Registry API enabled.${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 3: Cloning the java-docs-samples Repository                    ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Cloning the Java Docs Samples repository...${RESET_FORMAT}"
 
 git clone https://github.com/GoogleCloudPlatform/java-docs-samples
@@ -60,9 +51,6 @@ cd java-docs-samples/container-registry/container-analysis
 echo "${GREEN_TEXT}Repository cloned successfully and directory changed to container-analysis.${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔═════════════════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 4: Creating Container Dev Java Repository                          ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚═════════════════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Creating a Maven repository named container-dev-java-repo...${RESET_FORMAT}"
 
 gcloud artifacts repositories create container-dev-java-repo \
@@ -73,9 +61,6 @@ gcloud artifacts repositories create container-dev-java-repo \
 echo "${GREEN_TEXT}Container Dev Java Repository created successfully.${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔═════════════════════════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 5: Describing Container Dev Java Repository                                  ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚═════════════════════════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Describing the newly created repository...${RESET_FORMAT}"
 
 gcloud artifacts repositories describe container-dev-java-repo \
@@ -84,9 +69,6 @@ gcloud artifacts repositories describe container-dev-java-repo \
 echo "${GREEN_TEXT}Container Dev Java Repository described.${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔═══════════════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 6: Creating Maven Central Cache                                ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚═══════════════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Creating a remote Maven repository for caching Maven Central...${RESET_FORMAT}"
 
 gcloud artifacts repositories create maven-central-cache \
@@ -101,9 +83,6 @@ gcloud artifacts repositories create maven-central-cache \
 echo "${GREEN_TEXT}Maven Central Cache created successfully.${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔═══════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 7: Creating Policy file for the virtual repo                   ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚═══════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Creating the policy.json file for the virtual repository...${RESET_FORMAT}"
 
 cat > ./policy.json << EOF
@@ -125,9 +104,6 @@ EOF
 echo "${GREEN_TEXT}policy.json file created successfully.${RESET_FORMAT}"
 
 echo
-echo "${YELLOW_TEXT}${BOLD_TEXT}╔══════════════════════════════════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}                   Step 8: Creating the Virtual Maven Repository                         ${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}╚══════════════════════════════════════════════════════════════════════════════════════╝${RESET_FORMAT}"
 echo "${WHITE_TEXT}Creating the Virtual Maven Repository...${RESET_FORMAT}"
 echo "${WHITE_TEXT}Please wait this will take some time...${RESET_FORMAT}"
 
