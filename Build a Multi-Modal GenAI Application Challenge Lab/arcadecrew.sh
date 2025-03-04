f536fcbba4281d7c2954f8a9afa0ced4cb41960a
Arcade Crew <arcade8959@gmail.com>
2025-03-04 22:11:05 +0530

    Add files via upload

---
+++ Build a Multi-Modal GenAI Application Challenge Lab/arcadecrew.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# Bright Foreground Colors
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Displaying start message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}                  Starting the process...                   ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
+echo
+
+# Instruction for Region Input
+read -p "${BLUE_TEXT}${BOLD_TEXT}Enter REGION: ${RESET_FORMAT}" REGION
+echo
+
+# Displaying confirmation of user input
+echo "${GREEN_TEXT}${BOLD_TEXT}You have entered the region: ${REGION}${RESET_FORMAT}"
+echo
+
+ID="$(gcloud projects list --format='value(PROJECT_ID)')"
+
+cat > GenerateImage.py <<EOF_END
+import argparse
+
+import vertexai
+from vertexai.preview.vision_models import ImageGenerationModel
+
+def generate_image(
+    project_id: str, location: str, output_file: str, prompt: str
+) -> vertexai.preview.vision_models.ImageGenerationResponse:
+    """Generate an image using a text prompt.
+    Args:
+      project_id: Google Cloud project ID, used to initialize Vertex AI.
+      location: Google Cloud region, used to initialize Vertex AI.
+      output_file: Local path to the output image file.
+      prompt: The text prompt describing what you want to see."""
+
+    vertexai.init(project=project_id, location=location)
+
+    model = ImageGenerationModel.from_pretrained("imagegeneration@002")
+
+    images = model.generate_images(
+        prompt=prompt,
+        # Optional parameters
+        number_of_images=1,
+        seed=1,
+        add_watermark=False,
+    )
+
+    images[0].save(location=output_file)
+
+    return images
+
+generate_image(
+    project_id='$ID',
+    location='$REGION',
+    output_file='image.jpeg',
+    prompt='Create an image of a cricket ground in the heart of Los Angeles',
+    )
+
+EOF_END
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Generating an image... Please wait.${RESET_FORMAT}"
+/usr/bin/python3 /home/student/GenerateImage.py
+echo "${GREEN_TEXT}${BOLD_TEXT}Image generated successfully! Check 'image.jpeg' in your working directory.${RESET_FORMAT}"
+
+ID="$(gcloud projects list --format='value(PROJECT_ID)')"
+
+cat > genai.py <<EOF_END
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
+
+
+def generate_text(project_id: str, location: str) -> str:
+    # Initialize Vertex AI
+    vertexai.init(project=project_id, location=location)
+    # Load the model
+    multimodal_model = GenerativeModel("gemini-1.0-pro-vision")
+    # Query the model
+    response = multimodal_model.generate_content(
+        [
+            # Add an example image
+            Part.from_uri(
+                "gs://generativeai-downloads/images/scones.jpg", mime_type="image/jpeg"
+            ),
+            # Add an example query
+            "what is shown in this image?",
+        ]
+    )
+
+    return response.text
+
+
+project_id = "$ID"
+location = "$REGION"
+
+response = generate_text(project_id, location)
+print(response)
+
+EOF_END
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Processing text with multimodal model first time... Please wait.${RESET_FORMAT}"
+/usr/bin/python3 /home/student/genai.py
+echo "${GREEN_TEXT}${BOLD_TEXT}Text process completed, see output above.${RESET_FORMAT}"
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Waiting 30 seconds before running the process again...${RESET_FORMAT}"
+sleep 30
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Processing text with multimodal model second time... Please wait.${RESET_FORMAT}"
+/usr/bin/python3 /home/student/genai.py
+echo "${GREEN_TEXT}${BOLD_TEXT}Text process completed, see output above.${RESET_FORMAT}"
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}              Lab Completed Successfully!               ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
+echo
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
+echo
