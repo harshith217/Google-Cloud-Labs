2a4629ab33bbeb256a9b510def7e38a97b3059fe
Arcade Crew <arcade8959@gmail.com>
2025-03-19 09:12:10 +0530

    Add files via upload

--- Get Started with Sensitive Data Protection Challenge Lab/arcadecrew.sh
+++ Get Started with Sensitive Data Protection Challenge Lab/arcadecrew.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 # Define color variables
-YELLOW_TEXT=$'\033[0;33m'
-MAGENTA_TEXT=$'\033[0;35m'
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
 NO_COLOR=$'\033[0m'
-GREEN_TEXT=$'\033[0;32m'
-RED_TEXT=$'\033[0;31m'
-CYAN_TEXT=$'\033[0;36m'
-BOLD_TEXT=`tput bold`
-RESET_FORMAT=`tput sgr0`
-BLUE_TEXT=$'\033[0;34m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
 
-echo
-echo
 
-# Display initiation message
-echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
-
+clear
+# Welcome message
+echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
 echo
 
-# Display instruction for creating redact-request.json
-echo -e "${MAGENTA_TEXT}${BOLD_TEXT}Creating redact-request.json file...${RESET_FORMAT}"
+# Instruction 1
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 1: Creating redact-request.json file for de-identification.${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This file contains the data to be redacted and the configuration for the de-identification process.${RESET_FORMAT}"
+
 cat > redact-request.json <<EOF_END
 {
 	"item": {
@@ -47,20 +53,25 @@ cat > redact-request.json <<EOF_END
 }
 EOF_END
 
-# Display instruction for sending redact request
-echo -e "${YELLOW_TEXT}${BOLD_TEXT}Sending redact request to DLP API...${RESET_FORMAT}"
+# Instruction 2
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 2: Sending de-identification request to DLP API.${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This step sends the content of redact-request.json to the DLP API for de-identification.${RESET_FORMAT}"
+
 curl -s \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
   -H "Content-Type: application/json" \
   https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/content:deidentify \
   -d @redact-request.json -o redact-response.txt
 
-# Display instruction for uploading response to Google Cloud Storage
+# Copy response to Google Cloud Storage
 echo -e "${GREEN_TEXT}${BOLD_TEXT}Uploading redact-response.txt to Google Cloud Storage...${RESET_FORMAT}"
+
 gsutil cp redact-response.txt gs://$DEVSHELL_PROJECT_ID-redact
 
-# Display instruction for creating template.json
-echo -e "${MAGENTA_TEXT}${BOLD_TEXT}Creating structured_data_template.json file...${RESET_FORMAT}"
+# Instruction 3
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 3: Creating structured_data_template.${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This template defines how structured data should be de-identified.${RESET_FORMAT}"
+
 cat > template.json <<EOF_END
 {
 	"deidentifyTemplate": {
@@ -100,16 +111,20 @@ cat > template.json <<EOF_END
   }
 EOF_END
 
-# Display instruction for sending structured_data_template to DLP API
-echo -e "${YELLOW_TEXT}${BOLD_TEXT}Sending structured_data_template to DLP API...${RESET_FORMAT}"
+# Send template to API
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 4: Sending structured_data_template to DLP API...${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This step sends the structured data template to the DLP API.${RESET_FORMAT}"
+
 curl -s \
 -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
 -H "Content-Type: application/json" \
 https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/deidentifyTemplates \
 -d @template.json
 
-# Display instruction for creating unstructured_data_template.json
-echo -e "${MAGENTA_TEXT}${BOLD_TEXT}Creating unstructured_data_template.json file...${RESET_FORMAT}"
+# Instruction 4
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 5: Creating unstructured_data_template.${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This template defines how unstructured data should be de-identified.${RESET_FORMAT}"
+
 cat > template.json <<'EOF_END'
 {
   "deidentifyTemplate": {
@@ -147,8 +162,10 @@ cat > template.json <<'EOF_END'
 }
 EOF_END
 
-# Display instruction for sending unstructured_data_template to DLP API
-echo -e "${YELLOW_TEXT}${BOLD_TEXT}Sending unstructured_data_template to DLP API...${RESET_FORMAT}"
+# Send template to API
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 6: Sending unstructured_data_template to DLP API...${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This step sends the unstructured data template to the DLP API.${RESET_FORMAT}"
+
 curl -s \
 -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
 -H "Content-Type: application/json" \
@@ -157,24 +174,358 @@ https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/deidentifyTemplates 
 
 # Output the URLs for the templates
 echo -e "${GREEN_TEXT}${BOLD_TEXT}Structured Data Template URL:${RESET_FORMAT}"
+
 echo -e "${BLUE_TEXT}https://console.cloud.google.com/security/sensitive-data-protection/projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/structured_data_template/edit?project=$DEVSHELL_PROJECT_ID${RESET_FORMAT}"
 
 echo -e "${GREEN_TEXT}${BOLD_TEXT}Unstructured Data Template URL:${RESET_FORMAT}"
+
 echo -e "${BLUE_TEXT}https://console.cloud.google.com/security/sensitive-data-protection/projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/unstructured_data_template/edit?project=$DEVSHELL_PROJECT_ID${RESET_FORMAT}"
 
 # Display the message with colors
-echo -e "${CYAN_TEXT}${BOLD_TEXT}Now please follow the instructions provided in the video.${RESET}"
+echo -e "${CYAN_TEXT}${BOLD_TEXT}Now follow steps in video.${RESET_FORMAT}"
+echo
 
-# # Safely delete the script if it exists
-# SCRIPT_NAME="arcadecrew.sh"
-# if [ -f "$SCRIPT_NAME" ]; then
-#     echo -e "${BOLD_TEXT}${RED_TEXT}Deleting the script ($SCRIPT_NAME) for safety purposes...${RESET_FORMAT}${NO_COLOR}"
-#     rm -- "$SCRIPT_NAME"
-# fi
+read -p "${MAGENTA_TEXT}${BOLD_TEXT}Press Enter after completing above steps...${RESET_FORMAT}"
 
-# echo
-# echo
-# # Completion message
-# echo -e "${MAGENTA_TEXT}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
-# echo -e "${GREEN_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
+# Instruction 5
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 7: Creating job-configuration.json file for DLP job.${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This file contains the configuration for the DLP job, including the infoTypes to inspect and the actions to take.${RESET_FORMAT}"
+
+cat > job-configuration.json << EOM
+{
+  "triggerId": "dlp_job",
+  "jobTrigger": {
+    "triggers": [
+      {
+        "schedule": {
+          "recurrencePeriodDuration": "604800s"
+        }
+      }
+    ],
+    "inspectJob": {
+      "actions": [
+        {
+          "deidentify": {
+            "fileTypesToTransform": [
+              "TEXT_FILE",
+              "IMAGE",
+              "CSV",
+              "TSV"
+            ],
+            "transformationDetailsStorageConfig": {},
+            "transformationConfig": {
+              "deidentifyTemplate": "projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/unstructured_data_template",
+              "structuredDeidentifyTemplate": "projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/structured_data_template"
+            },
+            "cloudStorageOutput": "gs://$DEVSHELL_PROJECT_ID-output"
+          }
+        }
+      ],
+      "inspectConfig": {
+        "infoTypes": [
+          {
+            "name": "ADVERTISING_ID"
+          },
+          {
+            "name": "AGE"
+          },
+          {
+            "name": "ARGENTINA_DNI_NUMBER"
+          },
+          {
+            "name": "AUSTRALIA_TAX_FILE_NUMBER"
+          },
+          {
+            "name": "BELGIUM_NATIONAL_ID_CARD_NUMBER"
+          },
+          {
+            "name": "BRAZIL_CPF_NUMBER"
+          },
+          {
+            "name": "CANADA_SOCIAL_INSURANCE_NUMBER"
+          },
+          {
+            "name": "CHILE_CDI_NUMBER"
+          },
+          {
+            "name": "CHINA_RESIDENT_ID_NUMBER"
+          },
+          {
+            "name": "COLOMBIA_CDC_NUMBER"
+          },
+          {
+            "name": "CREDIT_CARD_NUMBER"
+          },
+          {
+            "name": "CREDIT_CARD_TRACK_NUMBER"
+          },
+          {
+            "name": "DATE_OF_BIRTH"
+          },
+          {
+            "name": "DENMARK_CPR_NUMBER"
+          },
+          {
+            "name": "EMAIL_ADDRESS"
+          },
+          {
+            "name": "ETHNIC_GROUP"
+          },
+          {
+            "name": "FDA_CODE"
+          },
+          {
+            "name": "FINLAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "FRANCE_CNI"
+          },
+          {
+            "name": "FRANCE_NIR"
+          },
+          {
+            "name": "FRANCE_TAX_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "GENDER"
+          },
+          {
+            "name": "GERMANY_IDENTITY_CARD_NUMBER"
+          },
+          {
+            "name": "GERMANY_TAXPAYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "HONG_KONG_ID_NUMBER"
+          },
+          {
+            "name": "IBAN_CODE"
+          },
+          {
+            "name": "IMEI_HARDWARE_ID"
+          },
+          {
+            "name": "INDIA_AADHAAR_INDIVIDUAL"
+          },
+          {
+            "name": "INDIA_GST_INDIVIDUAL"
+          },
+          {
+            "name": "INDIA_PAN_INDIVIDUAL"
+          },
+          {
+            "name": "INDONESIA_NIK_NUMBER"
+          },
+          {
+            "name": "IRELAND_PPSN"
+          },
+          {
+            "name": "ISRAEL_IDENTITY_CARD_NUMBER"
+          },
+          {
+            "name": "JAPAN_INDIVIDUAL_NUMBER"
+          },
+          {
+            "name": "KOREA_RRN"
+          },
+          {
+            "name": "MAC_ADDRESS"
+          },
+          {
+            "name": "MEXICO_CURP_NUMBER"
+          },
+          {
+            "name": "NETHERLANDS_BSN_NUMBER"
+          },
+          {
+            "name": "NORWAY_NI_NUMBER"
+          },
+          {
+            "name": "PARAGUAY_CIC_NUMBER"
+          },
+          {
+            "name": "PASSPORT"
+          },
+          {
+            "name": "PERSON_NAME"
+          },
+          {
+            "name": "PERU_DNI_NUMBER"
+          },
+          {
+            "name": "PHONE_NUMBER"
+          },
+          {
+            "name": "POLAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "PORTUGAL_CDC_NUMBER"
+          },
+          {
+            "name": "SCOTLAND_COMMUNITY_HEALTH_INDEX_NUMBER"
+          },
+          {
+            "name": "SINGAPORE_NATIONAL_REGISTRATION_ID_NUMBER"
+          },
+          {
+            "name": "SPAIN_CIF_NUMBER"
+          },
+          {
+            "name": "SPAIN_DNI_NUMBER"
+          },
+          {
+            "name": "SPAIN_NIE_NUMBER"
+          },
+          {
+            "name": "SPAIN_NIF_NUMBER"
+          },
+          {
+            "name": "SPAIN_SOCIAL_SECURITY_NUMBER"
+          },
+          {
+            "name": "STORAGE_SIGNED_URL"
+          },
+          {
+            "name": "STREET_ADDRESS"
+          },
+          {
+            "name": "SWEDEN_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "SWIFT_CODE"
+          },
+          {
+            "name": "THAILAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "TURKEY_ID_NUMBER"
+          },
+          {
+            "name": "UK_NATIONAL_HEALTH_SERVICE_NUMBER"
+          },
+          {
+            "name": "UK_NATIONAL_INSURANCE_NUMBER"
+          },
+          {
+            "name": "UK_TAXPAYER_REFERENCE"
+          },
+          {
+            "name": "URUGUAY_CDI_NUMBER"
+          },
+          {
+            "name": "US_BANK_ROUTING_MICR"
+          },
+          {
+            "name": "US_EMPLOYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "US_HEALTHCARE_NPI"
+          },
+          {
+            "name": "US_INDIVIDUAL_TAXPAYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "US_SOCIAL_SECURITY_NUMBER"
+          },
+          {
+            "name": "VEHICLE_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "VENEZUELA_CDI_NUMBER"
+          },
+          {
+            "name": "WEAK_PASSWORD_HASH"
+          },
+          {
+            "name": "AUTH_TOKEN"
+          },
+          {
+            "name": "AWS_CREDENTIALS"
+          },
+          {
+            "name": "AZURE_AUTH_TOKEN"
+          },
+          {
+            "name": "BASIC_AUTH_HEADER"
+          },
+          {
+            "name": "ENCRYPTION_KEY"
+          },
+          {
+            "name": "GCP_API_KEY"
+          },
+          {
+            "name": "GCP_CREDENTIALS"
+          },
+          {
+            "name": "JSON_WEB_TOKEN"
+          },
+          {
+            "name": "HTTP_COOKIE"
+          },
+          {
+            "name": "XSRF_TOKEN"
+          }
+        ],
+        "minLikelihood": "POSSIBLE"
+      },
+      "storageConfig": {
+        "cloudStorageOptions": {
+          "filesLimitPercent": 100,
+          "fileTypes": [
+            "TEXT_FILE",
+            "IMAGE",
+            "WORD",
+            "PDF",
+            "AVRO",
+            "CSV",
+            "TSV",
+            "EXCEL",
+            "POWERPOINT"
+          ],
+          "fileSet": {
+            "regexFileSet": {
+              "bucketName": "$DEVSHELL_PROJECT_ID-input",
+              "includeRegex": [],
+              "excludeRegex": []
+            }
+          }
+        }
+      }
+    },
+    "status": "HEALTHY"
+  }
+}
+EOM
+
+# Step 2: Send job configuration to DLP API
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Step 8: Sending job configuration to DLP API...${RESET_FORMAT}"
+echo -e "${CYAN_TEXT}This step sends the job configuration to the DLP API to create a new job trigger.${RESET_FORMAT}"
+
+curl -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/locations/global/jobTriggers \
+-d @job-configuration.json
+
+# Step 3: Wait for job trigger activation
+echo -e "${BLUE_TEXT}${BOLD_TEXT}Step 9: Waiting 15 seconds before activating the job trigger...${RESET_FORMAT}"
+sleep 15
+
+# Step 4: Activate the job trigger
+echo -e "${BLUE_TEXT}${BOLD_TEXT}Step 10: Activating the job trigger...${RESET_FORMAT}"
+
+curl --request POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "X-Goog-User-Project: $DEVSHELL_PROJECT_ID" \
+  "https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/locations/global/jobTriggers/dlp_job:activate"
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe our Channel:${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@Arcade61432${RESET_FORMAT}"
 echo
