3e062b13415f3fc52c5784e3a84b9653a01e5ae6
Arcade Crew <arcade8959@gmail.com>
2025-04-05 08:50:37 +0530

    Update arcadecrew.sh

--- Monitor an Apache Web Server using Ops Agent/arcadecrew.sh
+++ Monitor an Apache Web Server using Ops Agent/arcadecrew.sh
@@ -23,63 +23,137 @@ echo "${BLUE_TEXT}${BOLD_TEXT}         INITIATING EXECUTION...  ${RESET_FORMAT}"
 echo "${BLUE_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
 echo
 
-echo "${GREEN_TEXT}${BOLD_TEXT}Fetching the region...${RESET_FORMAT}"
-export REGION=$(gcloud container clusters list --format='value(LOCATION)')
+# Instruction for entering the zone
+echo "${YELLOW_TEXT}${BOLD_TEXT}Enter the Zone:${RESET_FORMAT}"
+read ZONE
 
-# Instruction for cluster credentials
-echo "${MAGENTA_TEXT}${BOLD_TEXT}Fetching credentials for the Kubernetes cluster...${RESET_FORMAT}"
-echo
+# Informing about VM creation
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Creating a VM instance named 'quickstart-vm' in the specified zone...${RESET_FORMAT}"
+gcloud compute instances create quickstart-vm --zone=$ZONE --machine-type=e2-small --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name=quickstart-vm,image=projects/debian-cloud/global/images/debian-11-bullseye-v20241009,mode=rw,size=10,type=pd-balanced
 
-gcloud container clusters get-credentials day2-ops --region $REGION
+# Informing about firewall rules
+echo "${CYAN_TEXT}${BOLD_TEXT}Setting up firewall rules to allow HTTP and HTTPS traffic...${RESET_FORMAT}"
+gcloud compute firewall-rules create allow-http-from-internet --target-tags=http-server --allow tcp:80 --source-ranges 0.0.0.0/0 --description="Allow HTTP from the internet"
 
-# Instruction for cloning the repository
-echo "${GREEN_TEXT}${BOLD_TEXT}Cloning the microservices demo repository...${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}This may take a few moments.${RESET_FORMAT}"
-echo
+gcloud compute firewall-rules create allow-https-from-internet --target-tags=https-server --allow tcp:443 --source-ranges 0.0.0.0/0 --description="Allow HTTPS from the internet"
 
-git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
+# Informing about script preparation
+echo "${GREEN_TEXT}${BOLD_TEXT}Preparing the disk setup script for Apache and Ops Agent installation...${RESET_FORMAT}"
+cat > prepare_disk.sh <<'EOF_END'
 
-cd microservices-demo
+sudo apt-get update && sudo apt-get install apache2 php7.0 -y
 
-# Instruction for deploying Kubernetes manifests
-echo "${YELLOW_TEXT}${BOLD_TEXT}Applying Kubernetes manifests to deploy the application...${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}Please wait while the resources are being created.${RESET_FORMAT}"
-echo
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-kubectl apply -f release/kubernetes-manifests.yaml
+# Configures Ops Agent to collect telemetry from the app and restart Ops Agent.
 
+set -e
+
+# Create a back up of the existing file so existing configurations are not lost.
+sudo cp /etc/google-cloud-ops-agent/config.yaml /etc/google-cloud-ops-agent/config.yaml.bak
+
+# Configure the Ops Agent.
+sudo tee /etc/google-cloud-ops-agent/config.yaml > /dev/null << EOF
+metrics:
+  receivers:
+    apache:
+      type: apache
+  service:
+    pipelines:
+      apache:
+        receivers:
+          - apache
+logging:
+  receivers:
+    apache_access:
+      type: apache_access
+    apache_error:
+      type: apache_error
+  service:
+    pipelines:
+      apache:
+        receivers:
+          - apache_access
+          - apache_error
+EOF
+
+sudo service google-cloud-ops-agent restart
 sleep 60
 
-# Instruction for retrieving the external IP
-echo "${MAGENTA_TEXT}${BOLD_TEXT}Retrieving the external IP of the frontend service...${RESET_FORMAT}"
-echo
+EOF_END
 
-export EXTERNAL_IP=$(kubectl get service frontend-external -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
-echo $EXTERNAL_IP
+# Informing about file transfer
+echo "${CYAN_TEXT}${BOLD_TEXT}Transferring the setup script to the VM instance...${RESET_FORMAT}"
+gcloud compute scp prepare_disk.sh quickstart-vm:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
 
-# Instruction for testing the application
-echo "${GREEN_TEXT}${BOLD_TEXT}Testing the application using curl...${RESET_FORMAT}"
-echo
+# Informing about script execution
+echo "${MAGENTA_TEXT}${BOLD_TEXT}Executing the setup script on the VM instance...${RESET_FORMAT}"
+gcloud compute ssh quickstart-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 
-curl -o /dev/null -s -w "%{http_code}\n"  http://${EXTERNAL_IP}
+# Informing about email channel creation
+echo "${CYAN_TEXT}${BOLD_TEXT}Creating an email notification channel...${RESET_FORMAT}"
+cat > email-channel.json <<EOF_END
+{
+  "type": "email",
+  "displayName": "ArcadeCrew",
+  "description": "Subscribe",
+  "labels": {
+    "email_address": "$USER_EMAIL"
+  }
+}
+EOF_END
 
-# Instruction for enabling analytics
-echo "${YELLOW_TEXT}${BOLD_TEXT}Enabling analytics for the default logging bucket...${RESET_FORMAT}"
-echo
+gcloud beta monitoring channels create --channel-content-from-file="email-channel.json"
 
-gcloud logging buckets update _Default \
-    --location=global \
-    --enable-analytics
+# Get the channel ID
+email_channel_info=$(gcloud beta monitoring channels list)
+email_channel_id=$(echo "$email_channel_info" | grep -oP 'name: \K[^ ]+' | head -n 1)
 
-# Instruction for creating a logging sink
-echo "${MAGENTA_TEXT}${BOLD_TEXT}Creating a logging sink for Kubernetes container logs...${RESET_FORMAT}"
-echo
+# Informing about alert policy creation
+echo "${YELLOW_TEXT}${BOLD_TEXT}Creating an alert policy for Apache traffic monitoring...${RESET_FORMAT}"
+cat > vm-alert-policy.json <<EOF_END
+{
+  "displayName": "Apache traffic above threshold",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - workload/apache.traffic",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"workload.googleapis.com/apache.traffic\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "60s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 4000
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "1800s",
+    "notificationPrompts": [
+      "OPENED"
+    ]
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [
+    "$email_channel_id"
+  ],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_END
 
-gcloud logging sinks create day2ops-sink \
-    logging.googleapis.com/projects/$DEVSHELL_PROJECT_ID/locations/global/buckets/day2ops-log \
-    --log-filter='resource.type="k8s_container"' \
-    --include-children \
-    --format='json'
+# Create the alert policy
+gcloud alpha monitoring policies create --policy-from-file=vm-alert-policy.json
 
 # Completion Message
 echo
