ec7b6a0535d809dbd79a0a3d727ea3ec8c380303
Arcade Crew <arcade8959@gmail.com>
2025-04-06 07:18:44 +0530

    Update arcadecrew.sh

--- Monitor an Apache Web Server using Ops Agent/arcadecrew.sh
+++ Monitor an Apache Web Server using Ops Agent/arcadecrew.sh
@@ -26,7 +26,7 @@ gcloud compute instances create quickstart-vm --project=$DEVSHELL_PROJECT_ID --z
 
 cat > cp_disk.sh <<'EOF_CP'
 
-sudo apt-get update && sudo apt-get install apache2 php7.0 -y
+sudo apt-get update && sudo apt-get install apache2 php
 
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
